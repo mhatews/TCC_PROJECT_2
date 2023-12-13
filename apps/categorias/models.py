a46e0478b2c08436fc405ab1f7bfedcf51949194
Matheus P. Oliveira <mat.p.oliveira@gmail.com>
2023-12-13 16:59:49 -0300

    16:59 13-12

--- apps/categorias/models.py
+++ apps/categorias/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=50)
-    user = models.OneToOneField(User, on_delete=models.PROTECT, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
