d08e871fd3e90244ffc7d54907272a3832a3e93f
Matheus P. Oliveira <mat.p.oliveira@gmail.com>
2023-12-13 14:07:42 -0300

    DEPLOY COMMIT

--- comserv/settings.py
+++ comserv/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7mqnctfa83ney(z6^dnlra(jwyvnz%hdcajd-1uq8cyqd=&oy3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'comserv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -100,11 +100,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
 
-#DATABASES = {
-  # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-#}
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
